916e7cac692ba835ecad6e1649a6cf3a75a1bdc8
Marek Kasztelnik <mkasztelnik@gmail.com>
2015-02-22 19:02:14 +0100

    Email configuration

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,4 +76,18 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.default_url_options = { host: ENV['HOST'] }
+  config.action_mailer.smtp_settings = {
+    address:              'mail.uj.edu.pl',
+    port:                 465,
+    user_name:            ENV['EMAIL_USERNAME'],
+    password:             ENV['EMAIL_PASSWORD'],
+    authentication:       :plain,
+    enable_starttls_auto: true,
+    tls:                  true
+  }
 end
