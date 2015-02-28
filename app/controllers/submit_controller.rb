8bfa03177759c6188522adb9eb351ae8d236e217
Marek Kasztelnik <mkasztelnik@gmail.com>
2015-02-28 19:38:17 +0100

    Don't use html elements in flash message

--- app/controllers/submit_controller.rb
+++ app/controllers/submit_controller.rb
@@ -4,9 +4,9 @@ class SubmitController < ApplicationController
     if current_user.application_ready? &&
       current_user.update_attributes(submitted: true)
 
-      redirect_to root_path, notice: I18n.t('application.submitted').html_safe
+      redirect_to root_path, notice: I18n.t('application.submitted')
     else
-      redirect_to root_path, alert: I18n.t('application.not_ready').html_safe
+      redirect_to root_path, alert: I18n.t('application.not_ready')
     end
   end
 end
