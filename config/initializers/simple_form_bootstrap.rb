52f57035f824e6f2f07ea96c345b2669034859da
Marek Kasztelnik <mkasztelnik@gmail.com>
2015-03-13 22:48:56 +0100

    Layout and texts improved

--- config/initializers/simple_form_bootstrap.rb
+++ config/initializers/simple_form_bootstrap.rb
@@ -13,9 +13,9 @@ SimpleForm.setup do |config|
     b.optional :readonly
     b.use :label, class: 'control-label'
 
-    b.use :hint,  wrap_with: { tag: 'p', class: 'help-block' }
     b.use :input, class: 'form-control'
     b.use :error, wrap_with: { tag: 'span', class: 'help-block' }
+    b.use :hint,  wrap_with: { tag: 'p', class: 'help-block' }
   end
 
   config.wrappers :vertical_file_input, tag: 'div', class: 'form-group', error_class: 'has-error' do |b|
