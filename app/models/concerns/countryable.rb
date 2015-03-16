9bade9c188af10934d50c7df9179e6e476ac2a9d
Marek Kasztelnik <mkasztelnik@gmail.com>
2015-03-16 10:41:09 +0100

    Country can be blank

--- app/models/concerns/countryable.rb
+++ app/models/concerns/countryable.rb
@@ -2,7 +2,7 @@ module Countryable
   extend ActiveSupport::Concern
 
   def country_name
-    if country
+    unless country.blank?
       c = ISO3166::Country[country]
       c.translations[I18n.locale.to_s] || c.name
     end
