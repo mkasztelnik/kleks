35da643b3e1018c7a2591e65537ae613e2fdf72b
Marek Kasztelnik <mkasztelnik@gmail.com>
2015-03-16 20:54:13 +0100

    Expire session on production after 20 minutes

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,12 @@
 # Be sure to restart your server when you modify this file.
 
-Rails.application.config.session_store :cookie_store, key: '_kleks_session'
+if Rails.env.production?
+  Rails.application.
+    config.session_store :cookie_store,
+                         key: '_kleks_session',
+                         expire_after: 20.minutes
+else
+  Rails.application.
+    config.session_store :cookie_store,
+                         key: '_kleks_session'
+end
