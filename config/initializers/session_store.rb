c5c8e44f79054083de0387a3354ce40174b9f881
Marek Kasztelnik <mkasztelnik@gmail.com>
2015-04-12 22:49:22 +0200

    Session timeout removed

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,12 +1,2 @@
 # Be sure to restart your server when you modify this file.
-
-if Rails.env.production?
-  Rails.application.
-    config.session_store :cookie_store,
-                         key: '_kleks_session',
-                         expire_after: 20.minutes
-else
-  Rails.application.
-    config.session_store :cookie_store,
-                         key: '_kleks_session'
-end
+Rails.application.config.session_store :cookie_store, key: '_kleks_session'
