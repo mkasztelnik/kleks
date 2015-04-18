c390e27112c141db15de6d6b53186efc074cd486
Marek Kasztelnik <mkasztelnik@gmail.com>
2015-04-18 13:41:24 +0200

    Deliver later

--- app/controllers/submit_controller.rb
+++ app/controllers/submit_controller.rb
@@ -2,7 +2,7 @@ class SubmitController < ApplicationController
   def submit
     if current_user.application_ready? &&
        current_user.update_attributes(submitted: true)
-      UserMailer.submit_email(current_user).deliver_now
+      UserMailer.submit_email(current_user).deliver_later
       redirect_to root_path, notice: I18n.t('application.submitted')
     else
       redirect_to root_path, alert: I18n.t('application.not_ready')
