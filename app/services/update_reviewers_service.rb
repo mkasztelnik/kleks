8ffc068732de18eed173bd05d7c620d442adce17
Marek Kasztelnik <mkasztelnik@gmail.com>
2015-05-08 12:35:26 +0200

    Fix problem with removing review while assigning reviewer

--- app/services/update_reviewers_service.rb
+++ app/services/update_reviewers_service.rb
@@ -7,12 +7,11 @@ class UpdateReviewersService
     applicants = User.applicants.includes(:reviews).references(:reviews)
     applicants.each do |applicant|
       reviewer_ids = @reviewers_hsh[applicant.id.to_s] || []
-      existing_reviewer_ids = applicant.reviews.map(&:reviewer_id)
+      existing_reviewer_ids = applicant.reviews.map(&:reviewer_id).map(&:to_s)
       to_add = reviewer_ids - existing_reviewer_ids
       to_remove = existing_reviewer_ids - reviewer_ids
 
-      applicant.reviews.where(reviewer_id: to_remove).
-        destroy_all
+      applicant.reviews.where(reviewer_id: to_remove).destroy_all
 
       to_add.each do |reviewer_id|
         applicant.reviews.create(reviewer_id: reviewer_id)
