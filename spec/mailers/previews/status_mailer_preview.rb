7e8d96afc7335f7a3efd4c692e5648c7ab486a58
Marek Kasztelnik <mkasztelnik@gmail.com>
2015-05-18 19:14:16 +0200

    Status email preview

--- spec/mailers/previews/status_mailer_preview.rb
+++ spec/mailers/previews/status_mailer_preview.rb
@@ -1,4 +1,10 @@
 # Preview all emails at http://localhost:3000/rails/mailers/status_mailer
 class StatusMailerPreview < ActionMailer::Preview
+  def reject_phd
+    StatusMailer.reject_email_phd(User.first)
+  end
 
+  def reject_ma_ba
+    StatusMailer.reject_email(User.first)
+  end
 end
